84672f689d946063b32dc511ba0fc0dbbe31bde1
Juan Anaya <juan.anayar@udea.edu.co>
2021-12-07 05:29:34 -0500

    Ejercicio terminado

--- Laboratorio 1A/Lab1A_Ejercicio06_AnayaJuan/main.cpp
+++ Laboratorio 1A/Lab1A_Ejercicio06_AnayaJuan/main.cpp
@@ -1,9 +1,37 @@
+/*
+ * Laboratorio 1A - Ejercicio 06
+ * Juan Sebastian Anaya Regino
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int base; // Esta variable almacenara la base para la operacion
+    int exponent; // Esta variable almacenara el exponente para la operacion
+    int result = 1; // Esta variable almacenara el resultado de base^exponent
+
+    // Se imprime la funcion del programa
+    cout << "Este programa recibe dos numeros enteros A y B e imprime el resuldado "
+            "de elevar A^B.\n" << endl;
+
+    // Se le pide al usuario que ingrese la base para la operacion
+    cout << "Ingrese la base A: ";
+    cin >> base;
+
+    // Se le pide al usuario que ingrese el exponente para la operacion
+    cout << "Ingrese el exponente B: ";
+    cin >> exponent;
+
+    // Se eleva 'base' a la 'exponent'
+    for(int aux=0; aux<exponent; ++aux){
+        result *= base;
+    }
+
+    // Se imprime el resultado de la operacion
+    cout << base << "^" << exponent << "=" << result << endl;
+
     return 0;
 }
