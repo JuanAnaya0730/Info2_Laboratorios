1c12c3d70d66a66c7e2551cc47e6ea127d379e24
Juan Anaya <juan.anayar@udea.edu.co>
2021-12-07 04:39:17 -0500

    Ejercicio terminado

--- Laboratorio 1A/Lab1A_Ejercicio01_AnayaJuan/main.cpp
+++ Laboratorio 1A/Lab1A_Ejercicio01_AnayaJuan/main.cpp
@@ -1,9 +1,31 @@
+/*
+ * Laboratorio 1A - Ejercicio 01
+ * Juan Sebastian Anaya Regino
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int dividend; // Variable que almacena el dividendo
+    int divisor; // Variable que almacena el divisor
+
+    // Se imprime la funcion del programa
+    cout << "Este programa recibe dos numeros enteros A y B "
+            "e imprime el residuo de la division A/B.\n" << endl;
+
+    // Se le pide al usuario que ingrese el valor para A (divisor)
+    cout << "Ingrese un numero para A: ";
+    cin >> dividend;
+
+    // Se le pide al usuario que ingrese el valor para B (dividendo)
+    cout << "Ingrese un numero para B: ";
+    cin >> divisor;
+
+    // Se imprime el residuo de A/B en pantalla
+    cout << "\nEl residuo de la division " << dividend << "/" << divisor << " es: " << dividend % divisor << endl;
+
     return 0;
 }
