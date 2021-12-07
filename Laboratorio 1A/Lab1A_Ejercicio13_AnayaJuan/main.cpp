ec22528650df6570d68d2c0c9953d07d9f971e72
Juan Anaya <juan.anayar@udea.edu.co>
2021-12-07 06:34:18 -0500

    Ejercicio terminado

--- Laboratorio 1A/Lab1A_Ejercicio13_AnayaJuan/main.cpp
+++ Laboratorio 1A/Lab1A_Ejercicio13_AnayaJuan/main.cpp
@@ -1,9 +1,30 @@
+/*
+ * Laboratorio 1A - Ejercicio 13
+ * Juan Sebastian Anaya Regino
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int numN; // Esta variable almacenara el numero ingresado
+
+    // Se imprime en pantalla la funcion del progrma
+    cout << "Este programa recibe un numero entero e imprime todos sus divisores.\n" << endl;
+
+    // Se le pide al usuario que ingrese un numero
+    cout << "Ingrese un numero: ";
+    cin >> numN;
+
+    /* Se imprimen en pantalla los divisores de N */
+    cout << "Los divisores de " << numN << " son:" << endl;
+    for(int aux=1; aux<=numN; ++aux){
+        if(!(numN%aux)){
+            cout << aux << endl;
+        }
+    }
+
     return 0;
 }
