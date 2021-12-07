d073e33f72ef32e7cd53896bdd7f86aa5f928cbf
Juan Anaya <juan.anayar@udea.edu.co>
2021-12-07 06:08:40 -0500

    Ejercicio terminado

--- Laboratorio 1A/Lab1A_Ejercicio10_AnayaJuan/main.cpp
+++ Laboratorio 1A/Lab1A_Ejercicio10_AnayaJuan/main.cpp
@@ -1,9 +1,31 @@
+/*
+ * Laboratorio 1A - Ejercicio 10
+ * Juan Sebastian Anaya Regino
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int numN; // Esta variable almacenara el numero al que se le calcularan sus multiplos
+
+    // Se imprime en pantalla la funcion del programa
+    cout << "Este programa recibe un numero entero entre 1 y 100 e imprime en pantalla todos sus multiplos "
+            "entre 1 y 100.\n\n";
+
+    // Se le pide al usuario que ingrese un numero
+    cout << "Ingrese un numero: ";
+    cin >> numN;
+
+    // Se imprimen en pantalla los multiplos de numN menores que 100
+    cout << "\nMultiplos de " << numN << " menores que 100 son:" << endl;
+
+    // Este ciclo se encarga de calcular y imprimir cada uno de los multiplos de numN menores que 100
+    for(int aux=1; aux<100 && (numN*aux)<100; ++aux){
+        cout << numN*aux << endl;
+    }
+
     return 0;
 }
