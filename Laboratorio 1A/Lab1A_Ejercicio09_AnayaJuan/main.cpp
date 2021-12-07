ad6c1e1dc0bab39e00bf05424c32d373a73bb308
Juan Anaya <juan.anayar@udea.edu.co>
2021-12-07 05:56:38 -0500

    Ejercicio terminado

--- Laboratorio 1A/Lab1A_Ejercicio09_AnayaJuan/main.cpp
+++ Laboratorio 1A/Lab1A_Ejercicio09_AnayaJuan/main.cpp
@@ -1,9 +1,31 @@
+/*
+ * Laboratorio 1A - Ejercicio 09
+ * Juan Sebastian Anaya Regino
+*/
+
 #include <iostream>
 
 using namespace std;
 
+#define pi 3.1416 // Valor constante para π
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int radius; // Esta variable almacenara el radio del circulo
+
+    /* Se imprime en pantalla la funcion del programa */
+    cout << "Este programa recibe un numero N e imprime el perimetro y area de "
+            "un circulo con radio N.\n" << endl;
+
+    // Se le pide al usuario que ingrese el radio del circulo
+    cout << "Ingrese el radio del circulo: ";
+    cin >> radius;
+
+    // Se imprime en pantalla el perimetro usando la formula P=2πr
+    cout << "\nPerimetro: " << 2*pi*radius << endl;
+
+    // Se imprime en pantalla el area usando la formula A=π(r^2)
+    cout << "Area: " << pi*radius*radius << endl;
+
     return 0;
 }
