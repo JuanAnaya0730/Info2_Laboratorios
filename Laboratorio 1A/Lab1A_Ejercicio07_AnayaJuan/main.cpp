c5775add85fb3998ca55db60a65780bd26c3f191
Juan Anaya <juan.anayar@udea.edu.co>
2021-12-07 05:40:18 -0500

    Ejercicio terminado

--- Laboratorio 1A/Lab1A_Ejercicio07_AnayaJuan/main.cpp
+++ Laboratorio 1A/Lab1A_Ejercicio07_AnayaJuan/main.cpp
@@ -1,9 +1,32 @@
+/*
+ * Laboratorio 1A - Ejercicio 07
+ * Juan Sebastian Anaya Regino
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int numN; // Varible que almacenara el numero ingresado por el usuario
+    int total = 0; // Variable que almacenara el resultado de la sumatoria
+
+    // Se imprime la funcion del programa
+    cout << "Este programa recibe un numero entero N e imprime la suma de todos los "
+            "numeros entre 0 y N, incluyendo N.\n" << endl;
+
+    // Se le pide al usuario que ingrese un numero
+    cout << "Ingrese un numero: ";
+    cin >> numN;
+
+    // Se suman todos los numeros entre 0 y el valor de numN
+    for(int aux=1; aux<=numN; ++aux){
+        total += aux;
+    }
+
+    // Se imprime el resultado de la sumatoria
+    cout << "\nLa sumatoria desde 0 hasta " << numN << " es: " << total << endl;
+
     return 0;
 }
