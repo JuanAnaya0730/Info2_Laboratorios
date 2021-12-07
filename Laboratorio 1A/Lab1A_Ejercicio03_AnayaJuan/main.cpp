fb6aca361c3518c147c857cf8483e614787bda9f
Juan Anaya <juan.anayar@udea.edu.co>
2021-12-07 04:58:09 -0500

    Ejercicio terminado

--- Laboratorio 1A/Lab1A_Ejercicio03_AnayaJuan/main.cpp
+++ Laboratorio 1A/Lab1A_Ejercicio03_AnayaJuan/main.cpp
@@ -1,9 +1,35 @@
+/*
+ * Laboratorio 1A - Ejercicio 03
+ * Juan Sebastian Anaya Regino
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    float numA; // Esta variable almacena el 1er numero
+    float numB; // Esta variable almacena el 2do numero
+
+    // Se imprime la funcion del programa
+    cout << "Este programa recibe dos numeros diferentes e "
+            "imprime en pantalla cual es el mayor.\n" << endl;
+
+    // Se le pide al usuario que ingrese el el primer numero
+    cout << "Ingrese el 1er numero: ";
+    cin >> numA;
+
+    // Se le pide al usuario que ingrese el segundo numero
+    cout << "Ingrese el 2do numero: ";
+    cin >> numB;
+
+    // Se verfica que numero es mayor
+    if(numA>numB){
+        cout << "\nEl mayor es " << numA << endl;
+    }else{
+        cout << "\nEl mayor es " << numB << endl;
+    }
+
     return 0;
 }
