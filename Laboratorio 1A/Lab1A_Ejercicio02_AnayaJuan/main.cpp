370b46a4e9543bbeb1a3f7385e643c3b89ce2c1a
Juan Anaya <juan.anayar@udea.edu.co>
2021-12-07 04:47:17 -0500

    Ejercicio terminado

--- Laboratorio 1A/Lab1A_Ejercicio02_AnayaJuan/main.cpp
+++ Laboratorio 1A/Lab1A_Ejercicio02_AnayaJuan/main.cpp
@@ -1,9 +1,30 @@
+/*
+ * Laboratorio 1A - Ejercicio 02
+ * Juan Sebastian Anaya Regino
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int num; // Variable que almacena el numero a verificar
+
+    // Se imprime la funcion del programa
+    cout << "Este programa recibe un numero entero e imprime en panatalla si es un"
+            "numer par o impar.\n" << endl;
+
+    // Se le pide al usuario que ingrese un numero
+    cout << "Ingrese un numero: ";
+    cin >> num;
+
+    // Si el residuo de dividir ese numero entre dos es cero, significa que es par.
+    if(!(num%2)){
+        cout << num << " es par." << endl;
+    }else{
+        cout << num << " es impar." << endl;
+    }
+
     return 0;
 }
