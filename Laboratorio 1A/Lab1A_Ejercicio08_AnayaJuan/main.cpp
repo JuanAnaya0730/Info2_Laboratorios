8a8c57cb1d531310505cb45b5c77f116e19f3276
Juan Anaya <juan.anayar@udea.edu.co>
2021-12-07 05:46:49 -0500

    Ejercicio terminado

--- Laboratorio 1A/Lab1A_Ejercicio08_AnayaJuan/main.cpp
+++ Laboratorio 1A/Lab1A_Ejercicio08_AnayaJuan/main.cpp
@@ -1,9 +1,31 @@
+/*
+ * Laboratorio 1A - Ejercicio 08
+ * Juan Sebastian Anaya Regino
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int numN; // Esta variable almacenara el numero al que se le calculara el factorial
+    int result = 1; // esta variable almacenara el factorial del numero que se ingreso
+
+    // Se imprime en pantalla la funcion del programa
+    cout << "Este programa recibe un numero entero e imprime en pantalla su factorial.\n" << endl;
+
+    // Se le pide al usuario que ingrese un numero
+    cout << "Ingrese un numero: ";
+    cin >> numN;
+
+    // Aqui se multiplican todos los numeros entre 1 y el valor de numN
+    for(int aux=1; aux<=numN; ++aux){
+        result *= aux;
+    }
+
+    // Se imprime en pantalla el factorial de numN
+    cout << numN << "!=" << result << endl;
+
     return 0;
 }
